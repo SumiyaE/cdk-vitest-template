2878c48ce3e258348c59da532e4246f1f0c6ec87
江口純矢 <s_eguchi@codmon.co.jp>
2025-04-26 07:54:46 +0900

    cdk init sample-app --language=typescriptの実行

---
+++ lib/cdk-vitest-template-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkVitestTemplateStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkVitestTemplateQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}

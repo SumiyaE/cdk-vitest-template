b8537640337393f8c24d7c66bcdce68a025f2ce5
江口純矢 <s_eguchi@codmon.co.jp>
2025-04-29 08:05:15 +0900

    test: snapshot testを追加

--- test/cdk-vitest-template.test.ts
+++ test/cdk-vitest-template.test.ts
@@ -1,17 +1,16 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkVitestTemplate from '../lib/cdk-vitest-template-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as CdkVitestTemplate from '../lib/cdk-vitest-template-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-vitest-template-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkVitestTemplate.CdkVitestTemplateStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkVitestTemplate.CdkVitestTemplateStack(app, 'MyTestStack');
+  // THEN
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  // 生成したテンプレートとスナップショットが同じか検証
+  expect(template).toMatchSnapshot();
 });

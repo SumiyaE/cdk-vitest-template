5fbf2d653e56374beeb238134b5c1520dc0eef97
江口純矢 <s_eguchi@codmon.co.jp>
2025-04-29 19:41:52 +0900

    fix: 改行

--- test/vitest.config.ts
+++ test/vitest.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  root: ".",
+  test: {
     root: ".",
-    test: {
-        root: ".",
-        environment: "node",
-        include: ["**/*.{test,spec}.ts"],
-    }
+    environment: "node",
+    include: ["**/*.{test,spec}.ts"],
+  }
 });
